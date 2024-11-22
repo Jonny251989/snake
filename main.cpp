0c2453525dd259779e37121f7606dfabe80a2646
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-23 00:36:22 +0300

    adding some remove

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "screencontroller.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
     ScreenController controller;
     controller.show();
